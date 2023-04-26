18b6515dfd87c89e7e2c82734977a5fe304a8654
BrahmaniThulluru <brahmani1247@gmail.com>
2023-04-26 16:47:13 +0530

    first commit

---
+++ djangoproject/mysite/ecom/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from ecom.views import index,details,cart_view,checkout,return_view,cancel_view
+
+app_name='ecom'
+
+urlpatterns = [
+    path('',index,name='index'),
+    path('<int:product_id>/<slug:slug>/',details,name = 'details'),
+    path('cart/',cart_view, name = 'cart_view'),
+    path('checkout/',checkout, name = 'checkout'),
+    path('success/',return_view, name = 'return_view'),
+    path('cancel/',cancel_view, name = 'cancel_view'),
+]
